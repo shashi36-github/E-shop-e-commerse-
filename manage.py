c82fe101e01685a6b2efbbef766ec6aa7a16df54
SUDANA SHASHI KIRAN <141379562+shashi36-github@users.noreply.github.com>
2024-06-25 11:19:23 +0530

    Add files via upload

---
+++ manage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eCommerce.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        sys.exit(1)  # Exit with non-zero status on error
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
